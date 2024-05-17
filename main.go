ff3a027305bc2f6c38af5fe6b7bac9238261a26a
Burak <148650898+BGNDGN@users.noreply.github.com>
2024-05-17 13:54:07 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"golang/internal" // 'yourmodule' yerine modül adınızı kullanın
+	"golang/internal" 
 	"net/http"
 )
 
 func main() {
-	// counterService'i başlatın
 	internal.CounterServiceInstance = internal.NewCounterService()
 
 	http.HandleFunc("/increment", internal.IncrementHandler)
 	http.HandleFunc("/decrement", internal.DecrementHandler)
-
-	// Sunucuyu başlatın
+	
 	http.ListenAndServe(":8080", nil)
 }
