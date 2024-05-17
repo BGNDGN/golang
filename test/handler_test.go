bca6aba686f642771c331e7e472f210ef163292d
Burak <148650898+BGNDGN@users.noreply.github.com>
2024-05-17 13:55:11 +0300

    Update handler_test.go

--- test/handler_test.go
+++ test/handler_test.go
@@ -40,27 +40,22 @@ func TestIncrementHandler(t *testing.T) {
 func TestDecrementHandler(t *testing.T) {
 
 	req := httptest.NewRequest("GET", "http://localhost:8080/decrement", nil)
-
-	// Create a mock HTTP response recorder
+	
 	w := httptest.NewRecorder()
 
-	// Call the handler function with the mock request and response
 	internal.DecrementHandler(w, req)
 
-	// Check the response status code (should be 200 OK)
 	if w.Code != http.StatusOK {
 		t.Errorf("Expected status code %d, got %d", http.StatusOK, w.Code)
 	}
 
-	// Parse the response body
 	var response internal.CounterResponse
 	err := json.Unmarshal(w.Body.Bytes(), &response)
 	if err != nil {
 		t.Errorf("Error decoding JSON response: %v", err)
 	}
 
-	// Check the counter value in the response
-	expectedCounter := -1 // Decrement işlemi sonrası değer 0 olmalı
+	expectedCounter := -1 // Decrement işlemi sonrası değer "0" olmalı.
 	if response.Counter != expectedCounter {
 		t.Errorf("Expected counter value %d, got %d", expectedCounter, response.Counter)
 	}
