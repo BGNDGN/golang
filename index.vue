e21d69fd77f97c2b7aa480fddb903b0f7c681b61
Burak <148650898+BGNDGN@users.noreply.github.com>
2024-05-17 13:29:25 +0300

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <button class="increment-btn" title="buton1" @click="arttirma">+</button> 
+    <button class="decrement-btn" title="buton2" @click="azaltma">-</button> 
+    <p>{{ counter }}</p> 
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      counter: 0 
+    };
+  },
+  methods: {
+    async arttirma() {
+      await this.counterarttir();
+    },
+    async azaltma() {
+      await this.counterazalt();
+    },
+
+    async counterarttir() {
+      axios.get('http://localhost:8080/increment')
+        .then(response => {
+          this.counter = response.data.counter; 
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error('İstek hatası:', error);
+        });
+    },
+
+    async counterazalt() {
+      axios.get('http://localhost:8080/decrement')
+        .then(response => {
+          this.counter = response.data.counter; 
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error('İstek hatası:', error);
+        });
+    }
+  }
+};
+</script>
